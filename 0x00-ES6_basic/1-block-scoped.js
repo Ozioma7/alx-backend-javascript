6143e49d3795c208e157417f463e537367e52819
Ozioma7 <okonisaozioma@gmail.com>
2024-03-07 02:51:50 -0800

    used const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Changed var to let
-  let task2 = true; // Changed var to let
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Removed var
-    task2 = false; // Removed var
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
