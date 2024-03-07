fde3fa234baca6834bd24914a011c674b387924b
Ozioma7 <okonisaozioma@gmail.com>
2024-03-07 10:30:04 +0100

    task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,13 @@
+function taskFirst() {
+    const a = 5;
+    const b = 10;
+    const sum = a + b;
+    console.log(sum);
+}
+
+function taskNext() {
+    let x = 5;
+    let y = 10;
+    let product = x * y;
+    console.log(product);
+}
