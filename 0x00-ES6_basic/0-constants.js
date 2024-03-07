05ca8711b995b3d60d6ec9a2d4efbc3d58902e41
Ozioma7 <okonisaozioma@gmail.com>
2024-03-07 10:44:10 +0100

    Amends

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,15 @@
-function taskFirst() {
-    const x = 5,
-          y = 10,
-          product = x * y;
-    console.log(product);
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-function taskNext() {
-    let x = 5,
-          y = 10,
-          product = x * y;
-    console.log(product);
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
