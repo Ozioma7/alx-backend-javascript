0b96ee34a6f6b4b6cce720e2cfe02ddaafe4ba5a
Ozioma7 <okonisaozioma@gmail.com>
2024-03-12 12:11:44 +0100

    8

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+  income: 'income-${getCurrentYear()}',
+  gdp: 'gdp-${getCurrentYear()}',
+  capita: 'capita-${getCurrentYear()}'
+};
+  return budget;
+}
