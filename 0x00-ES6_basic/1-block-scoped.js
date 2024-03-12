233bac726ae2f76319e811b1388e8c23cd76cde4
Ozioma7 <okonisaozioma@gmail.com>
2024-03-12 11:31:21 +0100

    5

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const task = trueOrFalse ? true : false;
-  const task2 = trueOrFalse ? false : true;
+  /* eslint no-shadow: "error" */
+  const task = false;
+  const task2 = true;
+  /*eslint-disable */
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
 
   return [task, task2];
 }
