d33b558d368d7af0827d53ce205765ec77670bee
Ozioma Michael <okonisaozioma@gmail.com>
2024-03-13 12:33:28 +0100

    ES6 ES6_classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import { Car } from './10-car.js';
+
+export class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
+
+    get range() {
+        return this._range;
+    }
+
+    cloneCar() {
+        // Create a new Car instance instead of EVCar
+        return new Car(this._brand, this._motor, this._color);
+    }
+}
